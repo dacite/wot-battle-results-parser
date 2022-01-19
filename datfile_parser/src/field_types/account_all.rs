554a33e95e5bc5c6074a9cceece7e29ae8c9c7c6
dacite <d4cite@outlook.com>
2022-01-18 23:14:10 -0800

    Modify account_all field type

--- datfile_parser/src/field_types/account_all.rs
+++ datfile_parser/src/field_types/account_all.rs
@@ -1,9 +1,13 @@
-pub const ACCOUNT_ALL: [(&str, &str); 7] = [
+pub const ACCOUNT_ALL: [(&str, &str); 6] = [
     ("avatarDamageDealt", "int"),
     ("avatarKills", "int"),
     ("avatarDamaged", "int"),
     ("totalDamaged", "int"),
     ("fairplayViolations", "tuple"),
     ("badges", "tuple"),
+];
+
+
+pub const ACCOUNT_ALL_SKIPPED: [(&str, &str); 1] = [
     ("playerRank", "int"),
 ];
