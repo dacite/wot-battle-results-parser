50c2c68ceae5c1c8d52d5a3883f88df1e9018295
dacite <d4cite@outlook.com>
2022-01-19 18:36:56 -0800

    Add player_info

--- datfile_parser/src/field_types/mod.rs
+++ datfile_parser/src/field_types/mod.rs
@@ -3,9 +3,11 @@ mod vehicle_self;
 mod vehicle_all;
 mod account_self;
 mod common;
+mod player_info;
 
 pub use account_all::ACCOUNT_ALL;
 pub use account_self::ACCOUNT_SELF;
 pub use vehicle_all::VEHICLE_ALL;
 pub use vehicle_self::VEHICLE_SELF;
 pub use common::COMMON;
+pub use player_info::PLAYER_INFO;
