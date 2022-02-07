589c23214a49adff1772214ca8f40af489a100c5
dacite <d4cite@outlook.com>
2022-02-06 21:36:42 -0800

    Add exec for testing

--- datfile_parser/src/main.rs
+++ datfile_parser/src/main.rs
@@ -4,7 +4,12 @@ fn main() {
     let path = "datfile_parser/examples/23984215712762303.dat";
     let file = std::fs::read(path).expect("unable to read the file");
 
-    wot_datfile_parser::parse(&file).unwrap();
+    let parser = wot_datfile_parser::DatFileParser::new();
+    let result = parser.parse(&file).unwrap();
+
+    let _s= serde_json::to_string_pretty(&result).unwrap();
+    // std::fs::write("result.json", s);
+
 }
 
 
