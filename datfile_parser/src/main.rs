0b4b9dc9c0c2f4f79b560cbad345f2c5f2b01da1
dacite <d4cite@outlook.com>
2022-02-15 22:35:45 -0800

    Support dirs

--- datfile_parser/src/main.rs
+++ datfile_parser/src/main.rs
@@ -2,14 +2,16 @@ fn main() {
     let paths = std::fs::read_dir("datfile_parser/examples").unwrap();
 
     for path in paths {
-        let file = std::fs::read(path.unwrap().path()).expect("unable to read the file");
+        if path.as_ref().unwrap().metadata().unwrap().is_file() {
+            let file = std::fs::read(path.unwrap().path()).expect("unable to read the file");
 
-        let parser = wot_datfile_parser::DatFileParser::new();
-        let result = parser.parse(&file).unwrap();
+            let parser = wot_datfile_parser::DatFileParser::new();
+            let result = parser.parse(&file).unwrap();
 
-        let s= serde_json::to_string_pretty(&result).unwrap();
+            let s = serde_json::to_string_pretty(&result).unwrap();
 
-        println!("{}", s);
+            println!("{}", s);
+        }
     }
     // std::fs::write("result.json", s);
 }
