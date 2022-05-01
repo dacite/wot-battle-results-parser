2d1d1ce3e66ea23ae01ea5b81d4eb42da26a8fdb
dacite <d4cite@outlook.com>
2022-04-30 23:06:45 -0700

    Bench to track parsing speed as events are added

--- replay_parser/benches/benchmark.rs
+++ replay_parser/benches/benchmark.rs
@@ -1,6 +1,8 @@
 use std::path::Path;
 
+use criterion::BenchmarkId;
 use criterion::{criterion_group, criterion_main, Criterion};
+use wot_replay_parser::event::BattleEvent;
 use wot_replay_parser::{parse, parse_json};
 
 
@@ -31,5 +33,25 @@ fn parse_entire_replay() {
     }
 }
 
-criterion_group!(benches, criterion_benchmark);
-criterion_main!(benches);
+fn parse_events(bin_stream: &[u8]) {
+    let stream = wot_replay_parser::packet_stream::PacketStream::new(bin_stream);
+    let packets: Vec<_> = stream.into_iter().collect();
+
+    let _events: Vec<_> = packets.iter().map(|packet| BattleEvent::new(packet)).collect();
+}
+
+pub fn criterion_benchmark_events(c: &mut Criterion) {
+    let file = std::fs::read("/home/dacite/Projects/wot-battle-results-parser/replay_parser/input_files/20220312_2330_uk-GB98_T95_FV4201_Chieftain_59_asia_great_wall.wotreplay").unwrap();
+    let result = parse(&file).unwrap();
+
+    let mut group = c.benchmark_group("Event parsing overhead");
+    group.sample_size(5000);
+    group.bench_with_input(BenchmarkId::new("input_example", "ok"), &result.1, |b, s| {
+        b.iter(|| parse_events(s));
+    });
+    group.finish();
+}
+
+// criterion_group!(benches, criterion_benchmark);
+criterion_group!(event_parsing, criterion_benchmark_events);
+criterion_main!(event_parsing);
