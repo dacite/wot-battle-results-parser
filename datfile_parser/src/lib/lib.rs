299b2a2d5d305daecb7174c2fbed51737a5e5509
dacite <d4cite@outlook.com>
2022-05-07 23:34:29 -0700

    Update comment width

--- datfile_parser/src/lib/lib.rs
+++ datfile_parser/src/lib/lib.rs
@@ -31,9 +31,9 @@ impl DatFileParser {
     }
 }
 
-/// The checksum describes the list of identifiers that are associated with that list of PickleValue. This prevents us
-/// from blindly assigning, for example `damageDealt` identifier to `PickleValue::I64(5433)` because `5433` looks like a
-/// `damageDealt` value. With checksum we can know for sure.
+/// The checksum describes the list of identifiers that are associated with that list of PickleValue. This
+/// prevents us from blindly assigning, for example `damageDealt` identifier to `PickleValue::I64(5433)`
+/// because `5433` looks like a `damageDealt` value. With checksum we can know for sure.
 fn get_checksum(data_list: &[PickleValue]) -> Result<i32> {
     let checksum = unpickler::access_i64(&data_list[0])?;
 
