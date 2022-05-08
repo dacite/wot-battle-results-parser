eb9a4fa461e12502db950959b6f8b74600afab2b
dacite <d4cite@outlook.com>
2022-05-07 23:34:57 -0700

    Add event debug macro

---
+++ macros/src/event_debug_derive.rs
@@ -0,0 +1,97 @@
+/// A macro that generates getters and setters for different structs (common,
+/// account_all etc.) inside the `standard_format` crate
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{Attribute, Data, DataStruct, Fields, Ident};
+
+pub fn imp_event_debug_macro(ast: &syn::DeriveInput) -> TokenStream {
+    let has_lifetime = if ast.generics.lt_token.is_some() { true } else { false };
+
+    let struct_name = &ast.ident;
+    let fields = match &ast.data {
+        Data::Struct(DataStruct {
+            fields: Fields::Named(fields),
+            ..
+        }) => &fields.named,
+        _ => panic!("expected a struct with named fields"),
+    };
+
+    let mut statements = Vec::new();
+
+    for field in fields {
+        let name = field.ident.clone().unwrap();
+        let attr_list = field.clone().attrs.into_iter().collect::<Vec<_>>();
+
+        let mut args = Vec::new();
+        for attr in attr_list {
+            if let Some(arg) = get_event_debug_arg(&attr) {
+                args.push(arg);
+            }
+        }
+
+        if args.is_empty() {
+            statements.push(quote! {
+                format!("{}: {:?} ", stringify!(#name), &self.#name)
+            });
+            continue;
+        }
+
+        for arg in args {
+            match arg.as_str() {
+                "ignore" => {}
+                "custom_debug" => {
+                    statements.push(quote! {
+                        format!("{}: {} ", stringify!(#name), &self.#name.to_debug_string(context))
+                    });
+                }
+                "as_player" => {
+                    statements.push(quote! {
+                        format!("{} {:?} ", stringify!(#name), &context.entity_id_to_player(self.#name).unwrap_or(self.#name.to_string()))
+                    });
+                }
+                _ => panic!("Unknown value given to event_printer_macro"),
+            }
+        }
+    }
+    let to_debug_string = quote! {
+        fn to_debug_string(&self, context: &crate::BattleContext) -> String
+            where
+                Self: std::fmt::Debug,
+            {
+                let mut output = String::new();
+                #(
+                    output.push_str(&#statements);
+                )*
+
+                output
+            }
+    };
+
+    let gen;
+
+    if has_lifetime {
+        gen = quote! {
+            impl EventPrinter for #struct_name<'_> {
+                #to_debug_string
+            }
+        };
+    } else {
+        gen = quote! {
+            impl EventPrinter for #struct_name {
+                #to_debug_string
+            }
+        };
+    }
+    gen.into()
+}
+
+fn get_event_debug_arg(attr: &Attribute) -> Option<String> {
+    if attr.path.is_ident("event_debug") {
+        let arg = attr.parse_args::<Ident>().unwrap();
+        return Some(arg.to_string());
+    } else {
+        return None;
+    }
+}
--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,5 +1,6 @@
 use proc_macro::TokenStream;
 
+mod event_debug_derive;
 mod field_access_derive;
 mod metadata_derive;
 
@@ -14,4 +15,10 @@ pub fn field_access_macro_derive(input: TokenStream) -> TokenStream {
 pub fn to_packet_macro_derive(input: TokenStream) -> TokenStream {
     let ast = syn::parse(input).unwrap();
     metadata_derive::imp_to_packet_macro(&ast)
+}
+
+#[proc_macro_derive(EventPrinter, attributes(event_debug))]
+pub fn to_event_printer_derive(input: TokenStream) -> TokenStream {
+    let ast = syn::parse(input).unwrap();
+    event_debug_derive::imp_event_debug_macro(&ast)
 }
