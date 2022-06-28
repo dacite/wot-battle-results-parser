6879b0ea137c134c32896ca5dfefcd56e013d5b9
dacite <d4cite@outlook.com>
2022-06-28 02:35:45 -0700

    SQL Insert Macro

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -3,6 +3,7 @@ use proc_macro::TokenStream;
 mod event_debug_derive;
 mod field_access_derive;
 mod metadata_derive;
+mod sql_insert_derive;
 mod version_derive;
 
 #[proc_macro_derive(FieldAccess, attributes(custom_parser))]
@@ -10,7 +11,6 @@ pub fn field_access_macro_derive(input: TokenStream) -> TokenStream {
     let ast = syn::parse(input).unwrap();
     field_access_derive::imp_field_access_macro(&ast)
 }
-
 
 #[proc_macro_derive(PacketMetadata)]
 pub fn to_packet_macro_derive(input: TokenStream) -> TokenStream {
@@ -28,4 +28,10 @@ pub fn to_event_printer_derive(input: TokenStream) -> TokenStream {
 pub fn to_version_derive(input: TokenStream) -> TokenStream {
     let ast = syn::parse(input).unwrap();
     version_derive::imp_version_macro(&ast)
+}
+
+#[proc_macro_derive(SqlInsert, attributes(version))]
+pub fn to_sql_insert_derive(input: TokenStream) -> TokenStream {
+    let ast = syn::parse(input).unwrap();
+    sql_insert_derive::imp_sql_insert_macro(&ast)
 }
