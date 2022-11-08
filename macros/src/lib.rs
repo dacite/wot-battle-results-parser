e21de861f64f8e94f024a5778f0c425f2b94d9d8
dacite <d4cite@outlook.com>
2022-11-07 22:50:38 -0800

    Specify explicit versions

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,22 +1,22 @@
 use proc_macro::TokenStream;
 
 mod event_debug_derive;
-mod field_access_derive;
-mod metadata_derive;
-mod sql_insert_derive;
+// mod field_access_derive;
+// mod metadata_derive;
+// mod sql_insert_derive;
 mod version_derive;
 
-#[proc_macro_derive(FieldAccess, attributes(custom_parser))]
-pub fn field_access_macro_derive(input: TokenStream) -> TokenStream {
-    let ast = syn::parse(input).unwrap();
-    field_access_derive::imp_field_access_macro(&ast)
-}
+// #[proc_macro_derive(FieldAccess, attributes(custom_parser))]
+// pub fn field_access_macro_derive(input: TokenStream) -> TokenStream {
+//     let ast = syn::parse(input).unwrap();
+//     field_access_derive::imp_field_access_macro(&ast)
+// }
 
-#[proc_macro_derive(PacketMetadata)]
-pub fn to_packet_macro_derive(input: TokenStream) -> TokenStream {
-    let ast = syn::parse(input).unwrap();
-    metadata_derive::imp_to_packet_macro(&ast)
-}
+// #[proc_macro_derive(PacketMetadata)]
+// pub fn to_packet_macro_derive(input: TokenStream) -> TokenStream {
+//     let ast = syn::parse(input).unwrap();
+//     metadata_derive::imp_to_packet_macro(&ast)
+// }
 
 #[proc_macro_derive(EventPrinter, attributes(event_debug))]
 pub fn to_event_printer_derive(input: TokenStream) -> TokenStream {
@@ -30,8 +30,8 @@ pub fn to_version_derive(input: TokenStream) -> TokenStream {
     version_derive::imp_version_macro(&ast)
 }
 
-#[proc_macro_derive(SqlInsert, attributes(version))]
-pub fn to_sql_insert_derive(input: TokenStream) -> TokenStream {
-    let ast = syn::parse(input).unwrap();
-    sql_insert_derive::imp_sql_insert_macro(&ast)
-}
+// #[proc_macro_derive(SqlInsert, attributes(version))]
+// pub fn to_sql_insert_derive(input: TokenStream) -> TokenStream {
+//     let ast = syn::parse(input).unwrap();
+//     sql_insert_derive::imp_sql_insert_macro(&ast)
+// }
