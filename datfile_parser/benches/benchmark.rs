69d891f1dbe47c7f7205a8fbeeedb3b7c791f169
dacite <d4cite@outlook.com>
2023-01-15 00:47:01 -0800

    Update datfile parser benchmark

--- datfile_parser/benches/benchmark.rs
+++ datfile_parser/benches/benchmark.rs
@@ -1,23 +1,17 @@
-use std::path::Path;
-
 use criterion::{criterion_group, criterion_main, Criterion};
 use rand::prelude::SliceRandom;
+use walkdir::WalkDir;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("parse dat file", |b| b.iter(parse_files));
 }
 
 fn parse_files() {
-    let all_entries: Vec<_> = utils::parse_dir(Path::new("input_files")).unwrap();
+    let all_entries = parse_dir("input_files");
     let entries: Vec<_> = all_entries.choose_multiple(&mut rand::thread_rng(), 5).collect();
 
     for entry in entries {
-        let path = entry.path();
-        if path.is_dir() {
-            continue;
-        }
-
-        let file = std::fs::read(path).expect("unable to read the file");
+        let file = std::fs::read(entry.path()).expect("unable to read the file");
 
         let parser = wot_datfile_parser::DatFileParser::new();
         let result = parser.parse(&file).unwrap();
@@ -28,3 +22,13 @@ fn parse_files() {
 
 criterion_group!(benches, criterion_benchmark);
 criterion_main!(benches);
+
+pub fn parse_dir(path: &str) -> Vec<walkdir::DirEntry> {
+    WalkDir::new(path)
+        .into_iter()
+        .filter_map(|entry| match entry {
+            Ok(entry) if entry.path().is_file() => Some(entry),
+            _ => None,
+        })
+        .collect()
+}
