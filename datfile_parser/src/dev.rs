dd09078e5a41cdcf79cecc5490a4c4bf5e5d1cf8
dacite <d4cite@outlook.com>
2023-01-15 00:47:13 -0800

    Fix datfile parse dev program

--- datfile_parser/src/dev.rs
+++ datfile_parser/src/dev.rs
@@ -2,16 +2,18 @@ use wot_datfile_parser::DatFileParser;
 fn main() {
     let parser = DatFileParser::new();
 
-    let intercept = |intercept_value, original_input| match intercept_value {
+    let intercept = |intercept_value, _original_input| match intercept_value {
         wot_datfile_parser::Intercept::Failed(field, default_val, err) => {
-            println!("Mannualy parsed : {}", original_input);
+            println!("Manually Parsed : {} Because: {err}", field.name);
+
             default_val
         }
         other @ _ => other.original_result(),
     };
+
     let battle = parser
         .parse_intercept(
-            &std::fs::read("datfile_parser/input_files/test/12570413167838342.dat").unwrap(),
+            &std::fs::read("datfile_parser/input_files/test/19011713064132879.dat").unwrap(),
             intercept,
         )
         .unwrap();
