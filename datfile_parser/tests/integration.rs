e4172882315f28b95325af8402b815a84ee99a75
dacite <d4cite@outlook.com>
2023-01-15 00:47:24 -0800

    Update integration test for datfile parser

--- datfile_parser/tests/integration.rs
+++ datfile_parser/tests/integration.rs
@@ -1,62 +1,40 @@
-use std::{fs, path::Path};
+use std::path::Path;
 
-use log::info;
+use walkdir::WalkDir;
 use wot_datfile_parser::{Battle, DatFileParser};
 
 #[cfg(test)]
 mod tests {
-    use std::path::Path;
-
     use wot_datfile_parser::DatFileParser;
+
+    use super::*;
 
     #[test]
     fn datfile_parser_test() {
-        env_logger::builder().is_test(true).try_init().unwrap();
         let parser = DatFileParser::new();
 
-        let mut battles = super::parse_dir(Path::new("input_files/test"), &parser);
-        battles.append(&mut super::parse_dir(Path::new("input_files/other"), &parser));
-        battles.append(&mut super::parse_dir(
-            Path::new("input_files/WOT_1_16_1_0"),
-            &parser,
-        ));
-        battles.append(&mut super::parse_dir(
-            Path::new("input_files/WOT_1_17_0_0"),
-            &parser,
-        ));
+        let datfiles = parse_dir("input_files");
 
-        battles
-            .into_iter()
-            .for_each(|battle| assert!(serde_json::to_string_pretty(&battle).is_ok()));
+        datfiles.into_iter().for_each(|datfile| {
+            let battle = parse_datfile(datfile.path(), &parser);
+            assert!(serde_json::to_string_pretty(&battle).is_ok())
+        });
     }
 }
 
-/// Parse a directory of .dat files (only direct childs of the directory)
-pub fn parse_dir(path: &Path, parser: &DatFileParser) -> Vec<Battle> {
-    let file_paths = fs::read_dir(path).unwrap();
-
-    let mut vec = Vec::new();
-
-    for path_result in file_paths {
-        match path_result {
-            Ok(path) => {
-                if path.path().is_file() {
-                    vec.push(parse_datfile(path.path().as_path(), parser));
-                }
-            }
-            Err(e) => {
-                println!("Failed to process DirEntry: {}", e);
-                continue;
-            }
-        }
-    }
-
-    vec
+pub fn parse_dir(path: &str) -> Vec<walkdir::DirEntry> {
+    WalkDir::new(path)
+        .into_iter()
+        .filter_map(|entry| match entry {
+            Ok(entry) if entry.path().is_file() => Some(entry),
+            _ => None,
+        })
+        .collect()
 }
 
 /// Parse a single .dat file
 pub fn parse_datfile(path: &Path, parser: &DatFileParser) -> Battle {
-    info!("Parsing {}", &path.to_string_lossy());
+    println!("Parsing {}", path.display());
     let file = std::fs::read(path).unwrap();
 
     parser.parse(&file).unwrap()
