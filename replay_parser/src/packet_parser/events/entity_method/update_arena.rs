b813410c99cef0ed695a54e257f36b9676b5d5ac
Gabouchet <gabriel.hamel.pro@gmail.com>
2023-02-14 16:18:21 +0100

    feat(arena): Added Period event

--- replay_parser/src/packet_parser/events/entity_method/update_arena.rs
+++ replay_parser/src/packet_parser/events/entity_method/update_arena.rs
@@ -1,6 +1,6 @@
 use nom::number::complete::le_u8;
 use serde_pickle::Value as PickleVal;
-use wot_types::{ArenaUpdate, AttackReason};
+use wot_types::{ArenaPeriod, ArenaUpdate, AttackReason, FinishReason};
 
 use crate::packet_parser::prelude::*;
 use crate::packet_parser::serde_packet;
@@ -18,6 +18,7 @@ pub enum UpdateData {
     VehicleKilled(VehicleKilled),
     BasePoints(BasePoints),
     BaseCaptured(BaseCaptured),
+    Period(Period),
     Unimplemented,
 }
 
@@ -39,6 +40,7 @@ impl UpdateArena {
             VehicleKilled => parse_vehicle_killed(arena_data)?,
             BasePoints => parse_base_points(arena_data)?,
             BaseCaptured => parse_base_captured(arena_data)?,
+            Period => parse_period(arena_data)?,
             _ => UpdateData::Unimplemented,
         };
 
@@ -292,4 +294,70 @@ fn parse_base_captured(arena_data: &[u8]) -> Result<UpdateData, PacketError> {
         team:    parse_value(0, &thing)?,
         base_id: parse_value(1, &thing)?,
     }))
+}
+
+#[derive(Debug, Clone, Serialize, Version)]
+pub struct ArenaEnded {
+    pub winner_team:   i32,
+    pub finish_reason: FinishReason,
+}
+
+#[derive(Debug, Clone, Serialize)]
+pub enum PeriodAdditionalInfo {
+    ActivitiesStartTimes(Vec<f32>),
+    ArenaEnded(ArenaEnded),
+}
+
+#[derive(Debug, Clone, Serialize, Version)]
+pub struct Period {
+    pub period:          ArenaPeriod,
+    pub end_time:        f32,
+    pub length:          f32,
+    pub additional_info: PeriodAdditionalInfo,
+}
+
+fn parse_period(arena_data: &[u8]) -> Result<UpdateData, PacketError> {
+    let decompressed =
+        utils::decompress_vec(arena_data, |err| PacketError::ConversionError(err.to_string()))?;
+    let pickle_value = serde_pickle::value_from_slice(
+        &decompressed,
+        serde_pickle::DeOptions::new().replace_unresolved_globals(),
+    )
+    .unwrap();
+
+    let PickleVal::Tuple(thing) = pickle_value else { todo!() };
+    let period_type: i32 = parse_value(0, &thing)?;
+
+    let additional_info = match &thing[3] {
+        PickleVal::Tuple(info) => {
+            let finish_reason = parse_value::<i32>(1, &info)?;
+            Ok(PeriodAdditionalInfo::ArenaEnded(ArenaEnded {
+                winner_team:   parse_value(0, &info)?,
+                finish_reason: FinishReason::try_from(finish_reason).map_err(|_| {
+                    PacketError::WrongEnumVariant(format!("finish reason of {finish_reason} is invalid"))
+                })?,
+            }))
+        }
+        PickleVal::List(start_times) => {
+            let mut res: Vec<f32> = vec![];
+            for i in 0..start_times.len() {
+                res.push(parse_value::<f32>(i, start_times)?);
+            }
+            Ok(PeriodAdditionalInfo::ActivitiesStartTimes(res))
+        }
+        _ => Err(PacketError::PickleError(format!(
+            "Invalid additional info payload"
+        ))),
+    }?;
+
+    let period = Period {
+        period: ArenaPeriod::try_from(period_type as i32).map_err(|_| {
+            PacketError::WrongEnumVariant(format!("arena period of {period_type} is invalid"))
+        })?,
+        end_time: parse_value(1, &thing)?,
+        length: parse_value(2, &thing)?,
+        additional_info,
+    };
+
+    Ok(UpdateData::Period(period))
 }
