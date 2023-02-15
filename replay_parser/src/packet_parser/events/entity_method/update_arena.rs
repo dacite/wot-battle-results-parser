9f85d84cef021ce8d710479a824da85f8ff7d9cc
Gabouchet <gabriel.hamel.pro@gmail.com>
2023-02-15 01:33:59 +0100

    feat(arena): Added Statistics event

--- replay_parser/src/packet_parser/events/entity_method/update_arena.rs
+++ replay_parser/src/packet_parser/events/entity_method/update_arena.rs
@@ -19,6 +19,7 @@ pub enum UpdateData {
     BasePoints(BasePoints),
     BaseCaptured(BaseCaptured),
     VehicleStatistics(VehicleStatistics),
+    Statistics(Vec<VehicleStatistics>),
     Unimplemented,
 }
 
@@ -41,6 +42,7 @@ impl UpdateArena {
             BasePoints => parse_base_points(arena_data)?,
             BaseCaptured => parse_base_captured(arena_data)?,
             VehicleStatistics => parse_vehicle_statistics(arena_data)?,
+            Statistics => parse_statistics(arena_data)?,
             _ => UpdateData::Unimplemented,
         };
 
@@ -319,4 +321,31 @@ fn parse_vehicle_statistics(arena_data: &[u8]) -> Result<UpdateData, PacketError
     };
 
     Ok(UpdateData::VehicleStatistics(vehicle_statistics))
+}
+
+fn parse_statistics(arena_data: &[u8]) -> Result<UpdateData, PacketError> {
+    let decompressed =
+        utils::decompress_vec(arena_data, |err| PacketError::ConversionError(err.to_string()))?;
+    let pickle_value = serde_pickle::value_from_slice(
+        &decompressed,
+        serde_pickle::DeOptions::new().replace_unresolved_globals(),
+    )
+    .unwrap();
+
+    let PickleVal::List(stats_list) = pickle_value  else {
+        return Err(PacketError::PickleError("Expected List value for payload".into()))
+    };
+
+    let mut res: Vec<VehicleStatistics> = vec![];
+    for stats in stats_list.into_iter() {
+        let PickleVal::Tuple(items) = stats  else {
+            return Err(PacketError::PickleError("Expected Tuple value for stats".into()))
+        };
+        res.push(VehicleStatistics {
+            vehicle_id: parse_value(0, &items)?,
+            frags:      parse_value(1, &items)?,
+        })
+    }
+
+    Ok(UpdateData::Statistics(res))
 }
